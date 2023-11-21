0c69f290e31e964891024a1ee2fec55c2e2e2686
Phornhathai <phornhathai.aum@gmail.com>
2023-11-21 14:47:18 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import axios from 'axios';
-import SampleData from './components/SampleData';
-import './App.css'
+import './App.css';
+
+// Use React.lazy to create a dynamic import of the SampleData component
+const SampleData = lazy(() => import('./components/SampleData'));
 
 const App = () => {
+  const [sampleData, setSampleData] = useState([]);
 
-  const [sampleData, setSampleData] = useState([])
-  
   useEffect(() => {
-    // ใช้ axios ดึงข้อมูล
+    // Use axios to fetch data
     axios.get('https://api.sampleapis.com/codingresources/codingResources')
       .then(res => {
-        // แสดงข้อมูลใน console
+        // Log data to console
         console.log(res.data);
 
-        // ตั้งค่าข้อมูลใน state
+        // Set data in state
         setSampleData(res.data);
       })
       .catch(error => {
         console.log(error);
       });
-  }, []); 
-  // ให้ useEffect ทำงานเมื่อ component ถูก mount เท่านั้น
+  }, []);
 
   return (
     <div id="app">
-    <h1 className='text-center'>Infinite Scroll</h1>
+      <h1 className='text-center'>Infinite Scroll</h1>
 
-      {/* ส่งค่า props sampleData ไปที่ components สำหรับแสดงข้อมูล */}
-      <SampleData sampleData={sampleData} />
-  </div>
+      {/* Use React.Suspense to wrap the lazy-loaded component */}
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* Pass the sampleData as a prop to the lazy-loaded component */}
+        <SampleData sampleData={sampleData} />
+      </Suspense>
+    </div>
   );
 };
 
