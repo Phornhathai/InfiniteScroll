80ef3f204548bc43653fae90f58892dec3d98de4
Phornhathai <phornhathai.aum@gmail.com>
2023-11-21 15:22:59 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import React, { useState, useEffect, lazy, Suspense } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-
-// Use React.lazy to create a dynamic import of the SampleData component
-const SampleData = lazy(() => import('./components/SampleData'));
+import SampleData from './components/SampleData';
 
 const App = () => {
   const [sampleData, setSampleData] = useState([]);
+  
 
   useEffect(() => {
     // Use axios to fetch data
     axios.get('https://api.sampleapis.com/codingresources/codingResources')
       .then(res => {
-        // Log data to console
-        console.log(res.data);
-
-        // Set data in state
-        setSampleData(res.data);
+       setSampleData(res.data)
       })
       .catch(error => {
         console.log(error);
@@ -26,12 +21,7 @@ const App = () => {
   return (
     <div id="app">
       <h1 className='text-center'>Infinite Scroll</h1>
-
-      {/* Use React.Suspense to wrap the lazy-loaded component */}
-      <Suspense fallback={<div>Loading...</div>}>
-        {/* Pass the sampleData as a prop to the lazy-loaded component */}
-        <SampleData sampleData={sampleData} />
-      </Suspense>
+      <SampleData sampleData={sampleData} />
     </div>
   );
 };
